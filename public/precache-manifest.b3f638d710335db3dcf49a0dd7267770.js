6cc804cf5e486c778cde3d7bb774a902f0f75a45
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2020-02-17 10:34:54 -0500

    Run yarn build on birch-book-search and put build files in public folder here

---
+++ public/precache-manifest.b3f638d710335db3dcf49a0dd7267770.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2d222e406e33e42e8363643a537084",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b36b06a20fbc0bc3c9fb",
+    "url": "/static/css/main.28b13197.chunk.css"
+  },
+  {
+    "revision": "f2b0578513c0cd7fac86",
+    "url": "/static/js/2.79f12072.chunk.js"
+  },
+  {
+    "revision": "b36b06a20fbc0bc3c9fb",
+    "url": "/static/js/main.47c036fc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9da5374caae0571eefad3b82f281fa5e.js"
+  "/precache-manifest.b3f638d710335db3dcf49a0dd7267770.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
